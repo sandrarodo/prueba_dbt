cda4d49270bf8c77c90df27e3a038c4b3f6489e5
sandra.rodriguez@sdggroup.com <sandra.rodriguez@sdggroup.com>
2024-05-18 17:41:58 +0000

    first changes (sources and stg_customer)

---
+++ models/staging/tpch/stg_customers.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+    select * from {{ source('tpch_sf1', 'customer') }}
+),
+
+customer as (
+    select 
+        c_custkey as customer_key,
+        c_name as customer_name,
+        c_address as customer_address,
+        c_nationkey as customer_nation_key,
+        c_phone as customer_phone,
+        c_acctbal as customer_acc_bal,
+        c_mktsegment as customer_mkt_segment,
+        c_comment as customer_comment
+    from source
+)
+
+select * from customer
